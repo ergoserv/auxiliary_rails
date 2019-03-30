c52078801c2e8c48f4f60650f32a35636c9ecadf
Dmitry Babenko <dmitry@ergoserv.com>
2019-03-30 10:28:14 +0200

    Generators: install rubocop

---
+++ lib/generators/auxiliary_rails/install_rubocop_generator.rb
@@ -0,0 +1,17 @@
+require 'rails'
+
+module AuxiliaryRails
+  class InstallRubocopGenerator < ::Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
+
+    def copy_config_file
+      copy_file "rubocop_template.yml", ".rubocop.yml"
+    end
+
+    def specify_gem_dependency
+      gem_group :development, :test do
+        gem "rubocop"
+      end
+    end
+  end
+end
