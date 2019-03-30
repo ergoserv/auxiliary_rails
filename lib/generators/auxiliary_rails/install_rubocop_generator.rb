805295b92f5b897661a224a7de1491eace39ca2f
Dmitry Babenko <dmitry@ergoserv.com>
2019-03-30 12:11:26 +0200

    Make rubocop config independent

--- lib/generators/auxiliary_rails/install_rubocop_generator.rb
+++ lib/generators/auxiliary_rails/install_rubocop_generator.rb
@@ -4,8 +4,11 @@ module AuxiliaryRails
   class InstallRubocopGenerator < ::Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
-    def copy_config_file
-      copy_file 'rubocop_template.yml', '.rubocop.yml'
+    def copy_config_files
+      copy_file 'rubocop_template.yml',
+                '.rubocop.yml'
+      copy_file 'rubocop_auxiliary_rails_template.yml',
+                '.rubocop_auxiliary_rails.yml'
     end
 
     def specify_gem_dependency
