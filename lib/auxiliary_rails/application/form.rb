502aacdf872a369bbff212f0c6b9906151c60373
Dmitry Babenko <dmitry@ergoserv.com>
2020-01-25 02:44:08 +0200

    Initial application Form implementation

---
+++ lib/auxiliary_rails/application/form.rb
@@ -0,0 +1,22 @@
+require 'active_model'
+
+module AuxiliaryRails
+  module Application
+    class Form
+      include ActiveModel::Attributes
+      include ActiveModel::AttributeAssignment
+      include ActiveModel::Model
+
+      class << self
+        # Method for ActiveModel::Translation
+        def i18n_scope
+          :forms
+        end
+      end
+
+      def submit
+        raise NotImplementedError
+      end
+    end
+  end
+end
