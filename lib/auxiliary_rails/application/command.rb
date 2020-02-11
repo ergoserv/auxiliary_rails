43d8abd7257d11f5ae3aee0a9291ce293a3ab920
Dmitry Babenko <dmitry@ergoserv.com>
2020-02-11 13:49:21 +0000

    Command - define `error_class` method on boot

--- lib/auxiliary_rails/application/command.rb
+++ lib/auxiliary_rails/application/command.rb
@@ -99,16 +99,15 @@ module AuxiliaryRails
         raise error_class, message
       end
 
-      # rubocop:disable Style/ClassVars
-      def error_class
-        @@error_class ||=
-          if defined?(ApplicationError)
-            ApplicationError
-          else
-            AuxiliaryRails::Application::Error
-          end
+      if defined?(ApplicationError)
+        def error_class
+          ApplicationError
+        end
+      else
+        def error_class
+          AuxiliaryRails::Application::Error
+        end
       end
-      # rubocop:enable Style/ClassVars
 
       def failure!(message = nil)
         ensure_empty_status!
