8c45c32efa89f0902ccb1de09bbe87d790181989
Dmitry Babenko <dmitry@ergoserv.com>
2020-02-17 18:29:20 +0200

    Remove if defined?(Rails)
    Gem is highly dependend on Rails

--- lib/auxiliary_rails.rb
+++ lib/auxiliary_rails.rb
@@ -1,7 +1,7 @@
 require 'auxiliary_rails/application/command'
 require 'auxiliary_rails/application/form'
 require 'auxiliary_rails/application/error'
-require 'auxiliary_rails/railtie' if defined?(Rails)
+require 'auxiliary_rails/railtie'
 require 'auxiliary_rails/version'
 
 module AuxiliaryRails
