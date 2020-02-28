156cb517ee0bccea95ffec2541e05c0acafe11f8
Dmitry Babenko <dmitry@ergoserv.com>
2020-02-29 00:01:30 +0200

    Specs for Command on Blocks

--- spec/auxiliary_rails/application/command_spec.rb
+++ spec/auxiliary_rails/application/command_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe AuxiliaryRails::Application::Command do
       expect { cmd.call }.to raise_error NotImplementedError
     end
   end
+
+  describe '#on' do
+    it 'provides access to caller methods' do
+      def example_method; end
+
+      SampleCommands::SuccessCommand.call
+        .on(:success) do
+          expect(self).to be_respond_to :example_method
+        end
+    end
+
+    it 'provides interface to command object inside block' do
+      SampleCommands::FailureWithErrorsCommand.call
+        .on(:failure) do |cmd|
+          expect(cmd.errors).to be_any
+        end
+    end
+  end
 end
