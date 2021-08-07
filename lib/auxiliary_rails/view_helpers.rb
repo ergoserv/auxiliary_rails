c62c9f9a2d785d8ff4fc66307ed598affc60e50e
Dmitry Babenko <dmitry@ergoserv.com>
2021-08-07 10:26:50 +0300

    View display_name helper

--- lib/auxiliary_rails/view_helpers.rb
+++ lib/auxiliary_rails/view_helpers.rb
@@ -1,5 +1,10 @@
+Dir[File.expand_path('view_helpers', __dir__) + '/*.rb']
+  .sort.each { |f| require f }
+
 module AuxiliaryRails
   module ViewHelpers
+    include DisplayHelper
+
     def current_controller?(*ctrl_names)
       ctrl_names.include?(params[:controller])
     end
---
+++ lib/auxiliary_rails/view_helpers/display_helper.rb
@@ -0,0 +1,30 @@
+module AuxiliaryRails
+  module ViewHelpers
+    module DisplayHelper
+      DISPLAY_NAME_METHODS = %i[
+        display_name
+        name
+        title
+        full_name
+        username
+        email
+      ].freeze
+
+      def display_name(resource)
+        return if resource.nil?
+
+        DISPLAY_NAME_METHODS.each do |method_name|
+          if resource.respond_to?(method_name)
+            return resource.public_send(method_name)
+          end
+        end
+
+        if resource.respond_to?(:model_name) && resource.respond_to?(:id)
+          "#{resource.model_name.human} ##{resource.id}"
+        else
+          resource.to_s
+        end
+      end
+    end
+  end
+end
