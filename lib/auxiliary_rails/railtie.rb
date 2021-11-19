4ce2e7dba4ad53fcd460077bf5e486de47c69370
Dmitry Babenko <dmitry@ergoserv.com>
2021-11-19 22:08:37 +0200

    Fix Lint/SendWithMixinArgument

--- lib/auxiliary_rails/railtie.rb
+++ lib/auxiliary_rails/railtie.rb
@@ -3,7 +3,7 @@ require 'auxiliary_rails/view_helpers'
 module AuxiliaryRails
   class Railtie < Rails::Railtie
     initializer 'auxiliary_rails.view_helpers' do
-      ActionView::Base.send :include, ViewHelpers
+      ActionView::Base.include ViewHelpers
     end
   end
 end
