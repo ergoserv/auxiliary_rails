85c72af22578ef5801b463729f1f553384892cb2
Eugene Mikhasenko <eug.mikhasenko@gmail.com>
2022-01-28 16:49:52 +0200

    Require resourceable in main file

--- lib/auxiliary_rails.rb
+++ lib/auxiliary_rails.rb
@@ -1,7 +1,8 @@
 require 'auxiliary_rails/application/command'
-require 'auxiliary_rails/application/form'
 require 'auxiliary_rails/application/error'
+require 'auxiliary_rails/application/form'
 require 'auxiliary_rails/application/query'
+require 'auxiliary_rails/concerns/resourceable'
 require 'auxiliary_rails/railtie'
 require 'auxiliary_rails/version'
 
